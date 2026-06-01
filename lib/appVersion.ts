982cdb71786ece18fc774cbb9057bfbb1002c271
joshgorrell <josh@ksav.com>
2026-06-01 12:11:19 -0500

    Updated appVersion.ts

--- lib/appVersion.ts
+++ lib/appVersion.ts
@@ -1,5 +1,5 @@
-export const APP_CODE_VERSION = 'pre-login-debug-2026-06-01-v6';
-export const OTA_MARKER = 'V6 PRE-LOGIN DEBUG';
+export const APP_CODE_VERSION = 'pre-login-debug-2026-06-01-v7';
+export const OTA_MARKER = 'V7 ANON KEY FIX';
 
 // Injected by EAS at build time via EXPO_PUBLIC_GIT_SHA env var.
 // Will be null in dev / older builds that predate this change.
