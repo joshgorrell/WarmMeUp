bbf055c4645c07339c07b905a6ce35c767e2e9f3
joshgorrell <josh@ksav.com>
2026-06-01 17:22:13 -0500

    Publish V8 recent activity OTA

--- lib/appVersion.ts
+++ lib/appVersion.ts
@@ -1,5 +1,5 @@
-export const APP_CODE_VERSION = 'pre-login-debug-2026-06-01-v7';
-export const OTA_MARKER = 'V7 ANON KEY FIX';
+export const APP_CODE_VERSION = 'pre-login-debug-2026-06-01-v8';
+export const OTA_MARKER = 'V8 RECENT ACTIVITY FIX';
 
 // Injected by EAS at build time via EXPO_PUBLIC_GIT_SHA env var.
 // Will be null in dev / older builds that predate this change.
